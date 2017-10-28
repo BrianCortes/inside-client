8c0fb9bedc50870552700654c22dc9896eba9e0a
Brian Cortes <brian@monoku.com>
2017-10-28 10:36:51 -0500

    feat(config): Add automatic react router

---
+++ src/components/pages/Author/Author.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Author = () => (
+  <div>
+    <h1>hello my friends</h1>
+  </div>
+);
+
+export default Author;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import { LocaleProvider } from 'antd';
 import enUS from 'antd/lib/locale-provider/en_US';
 import Home from 'components/pages/Home';
+import Author from 'components/pages/Author';
 
 import rootReducer from './reducers';
 import registerServiceWorker from './registerServiceWorker';
@@ -18,7 +20,12 @@ const store = createStore(
 ReactDOM.render(
   <Provider store={store}>
     <LocaleProvider locale={enUS}>
-      <Home />
+      <Router>
+        <div>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/author" component={Author} />
+        </div>
+      </Router>
     </LocaleProvider>
   </Provider>,
   document.getElementById('root'));
